2418480d0ef45e915d68565b83ce95f960026484
097yuji <g621308@gmail.com>
2019-02-20 15:33:26 +0900

    add_asynchronous-communication1

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function(){
+
+  function buildHTML(message){
+    var content = message.content ? `${message.content} ` : ''
+    var image = message.image.url ? `<img src='${message.image.url}'> ` : ''
+
+    var html = `<div class = "message-box" data-id=${message.id}>
+                  <div class = "message-box__name">
+                    ${message.user_name}
+                  </div>
+                  <div class = "message-box__time">
+                    ${message.created_at}
+                  </div>
+                  <p class = "lower-message__content">
+                    ${content}
+                    ${image}
+                  </p>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $(".messages").animate({scrollTop:$('.messages')[0].scrollHeight});
+      $('.form__message').val('');
+      $(".form__submit").removeAttr("disabled");
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
